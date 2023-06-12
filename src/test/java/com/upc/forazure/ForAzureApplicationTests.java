625aaab6afe66d97d0d0636857e8b55da7588ce0
ElizabetHuaroto <huarotocardenas@gmail.com>
2023-06-12 10:50:40 -0500

    first commit

---
+++ src/test/java/com/upc/forazure/ForAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.forazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
