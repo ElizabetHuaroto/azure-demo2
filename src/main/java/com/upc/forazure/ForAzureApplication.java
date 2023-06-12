1450631c3735217348b55b3975fa1d8e1e194fcc
ElizabetHuaroto <huarotocardenas@gmail.com>
2023-06-12 11:16:04 -0500

    first commit

--- src/main/java/com/upc/forazure/ForAzureApplication.java
+++ src/main/java/com/upc/forazure/ForAzureApplication.java
@@ -2,10 +2,17 @@ package com.upc.forazure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ForAzureApplication {
 
+    @GetMapping("/message")
+    public String message(){
+        return "felicitaciones si funciona";
+    }
     public static void main(String[] args) {
         SpringApplication.run(ForAzureApplication.class, args);
     }
